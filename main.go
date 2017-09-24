96d98d1e8ecabe99403fe80aa3804821336702a6
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-09-25 01:22:22 +0300

    server

--- main.go
+++ main.go
@@ -3,39 +3,97 @@ package main
 import (
 	"net"
 	"fmt"
-	"strconv"
 	"bufio"
+	"flag"
+	"os"
 )
 
+const DEFAULT_HOST = "127.0.0.1"
+const DEFAULT_PORT = 42042
+
 func main() {
-	port := 8080
+	host := *flag.String("host", DEFAULT_HOST, "Host")
+	port := *flag.Int("port", DEFAULT_PORT, "Port")
 
 	// create listening socket
-	socket, err := net.Listen("tcp", ":" + strconv.Itoa(port))
+	bindAddress := fmt.Sprintf("%s:%d", host, port)
+	socket, err := net.Listen("tcp", bindAddress)
 	if err != nil {
-		fmt.Print("Error starting server")
+		fmt.Println("Error starting server: ", err)
+		os.Exit(1)
+	} else {
+		fmt.Println(fmt.Sprintf("Server started on %s:%d", host, port))
 	}
 
+	// channel
+	channel := make(chan string)
+
+	// prepare connection pool
+	connectionId := 0
+	connectionPool := [10]net.Conn {}
+
 	// accept connections
-	for {
-		fmt.Println("Wait for incoming connection")
-		connection, err := socket.Accept()
-		if err != nil {
-			fmt.Print("Error accepting connection")
+	fmt.Println("Waiting for incoming connection")
+	go func() {
+		// wait to accept connection
+		for {
+			// accept connection
+			connection, err := socket.Accept()
+			if err != nil {
+				fmt.Print("Error accepting connection", err)
+				continue
+			}
+
+			// add connection to pool
+			connectionPool[connectionId] = connection
+			connectionId = connectionId + 1
+
+			// handle request
+			go func(channel chan string) {
+				fmt.Println(fmt.Sprintf("Accepting connection %d", connectionId))
+
+				// create reader
+				reader := bufio.NewReader(connection)
+
+				// wait for next read buffer ready
+				for {
+					// check if connection still alive
+					// ...
+
+					// read request
+					request, _, err := reader.ReadLine()
+					if err != nil {
+						fmt.Println("Error reading request stream", err)
+						// stop reading buffer and exit goroutine
+						break
+					} else {
+						fmt.Println(fmt.Sprintf("Connection %d accept message: %s", connectionId, string(request)))
+						// send request to channel
+						channel <- string(request)
+					}
+				}
+			}(channel)
 		}
-		fmt.Println("Connection accepted")
+	}()
 
+	// get request from channel
+	for {
+		// get request from channel
+		request := <- channel
+		fmt.Println("Request in channel:", request)
+
+		// send request to all connections
 		go func() {
-			// read request
-			request, _, _:= bufio.NewReader(connection).ReadLine()
-
 			// send response
-			writer:= bufio.NewWriter(connection)
-			writer.WriteString("Request: " + string(request) + "\n")
-			writer.Flush()
-
-			// close connection
-			connection.Close()
+			for targetConnectionId, targetConnection :=range connectionPool {
+				if connectionId == targetConnectionId {
+					continue
+				}
+				fmt.Println(fmt.Sprintf("Sending to connection %d", targetConnectionId))
+				writer:= bufio.NewWriter(targetConnection)
+				writer.WriteString(string(request) + "\n")
+				writer.Flush()
+			}
 		}()
 	}
 }

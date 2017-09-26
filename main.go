25e2947f3248b0c4359f7e0657ea9acc68df0619
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-09-26 22:19:28 +0300

    move main func

--- main.go
+++ main.go
@@ -18,6 +18,35 @@ type message struct {
 	sourceConnectionId int
 }
 
+func main() {
+	// declare command line options
+	host := flag.String("host", DEFAULT_HOST, "Host")
+	port := flag.Int("port", DEFAULT_PORT, "Port")
+	flag.Parse()
+
+	// create listening socket
+	bindAddress := fmt.Sprintf("%s:%d", *host, *port)
+	socket, err := net.Listen("tcp", bindAddress)
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+		os.Exit(1)
+	} else {
+		fmt.Println(fmt.Sprintf("Server started on %s:%d", *host, *port))
+	}
+
+	// channel
+	channel := make(chan message, MESSAGE_CHANNEL_LENGTH)
+
+	// prepare connection pool
+	connectionPool := server.NewConnectionPool()
+
+	// accept connections
+	go acceptConnections(channel, socket, connectionPool)
+
+	// publish request
+	publishMessage(channel, connectionPool)
+}
+
 // wait new connection on listening socket
 func acceptConnections(
 	messageChannel chan message,
@@ -93,33 +122,4 @@ func publishMessage(
 			})
 		}()
 	}
-}
-
-func main() {
-	// declare command line options
-	host := flag.String("host", DEFAULT_HOST, "Host")
-	port := flag.Int("port", DEFAULT_PORT, "Port")
-	flag.Parse()
-
-	// create listening socket
-	bindAddress := fmt.Sprintf("%s:%d", *host, *port)
-	socket, err := net.Listen("tcp", bindAddress)
-	if err != nil {
-		fmt.Println("Error starting server: ", err)
-		os.Exit(1)
-	} else {
-		fmt.Println(fmt.Sprintf("Server started on %s:%d", *host, *port))
-	}
-
-	// channel
-	channel := make(chan message, MESSAGE_CHANNEL_LENGTH)
-
-	// prepare connection pool
-	connectionPool := server.NewConnectionPool()
-
-	// accept connections
-	go acceptConnections(channel, socket, connectionPool)
-
-	// publish request
-	publishMessage(channel, connectionPool)
 }
